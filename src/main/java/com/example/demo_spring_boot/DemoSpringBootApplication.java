dc05c68dc85585d4e7f9dac8d36fc57bd7fc3820
manhdigi <manh.vv@digidinos.com>
2025-05-22 11:45:40 +0700

    add job and schedule

--- src/main/java/com/example/demo_spring_boot/DemoSpringBootApplication.java
+++ src/main/java/com/example/demo_spring_boot/DemoSpringBootApplication.java
@@ -2,8 +2,10 @@ package com.example.demo_spring_boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class DemoSpringBootApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/example/demo_spring_boot/job/ProductSyncJob.java
@@ -0,0 +1,21 @@
+package com.example.demo_spring_boot.job;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductSyncJob {
+
+    // Chạy mỗi 10 giây để demo
+//    @Scheduled(fixedRate = 10000)
+//    public void syncProductJob() {
+//        System.out.println("🕒 [ProductSyncJob] Syncing products with external system... " + System.currentTimeMillis());
+//        // Gọi service để thực hiện logic nếu cần
+//    }
+
+    // Ví dụ chạy 1 lần mỗi ngày lúc 2h sáng
+    @Scheduled(cron = "0 45 11 * * ?")
+    public void dailyCleanup() {
+        System.out.println("🧹 [ProductSyncJob] Running daily cleanup...");
+    }
+}
